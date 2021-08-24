4583c8bc1a1813c4ee67eccc5054c5c8e6b6a474
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-24 04:12:36 -0400

    style
    Styling done for the project

---
+++ main.js
@@ -0,0 +1,19 @@
+const initialPrice = document.querySelector("#initial-price");
+const currentPrice = document.querySelector("#current-price");
+const quantityStocks = document.querySelector("quantity-stocks");
+const submitBtn = document.querySelector("#submit-btn");
+
+
+
+
+function calculateProfitAndLoss(SellingPrice, CostPrice){
+  if(SellingPrice > CostPrice){
+      console.log(`the way he is a great ${ds}`)
+  }
+}
+
+
+
+
+
+submitBtn.addEventListener("click", calculateProfitAndLoss);
