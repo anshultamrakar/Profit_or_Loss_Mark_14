0350878407c71ed7ffb2af2c57c35accbebbe0b1
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-24 13:28:08 -0400

    Completed the mark 14
    Still have some bugs , Need to fixed

--- main.js
+++ main.js
@@ -5,15 +5,31 @@ const submitBtn = document.querySelector("#submit-btn");
 
 
 
+submitBtn.addEventListener("click", submitHandler);
 
-function calculateProfitAndLoss(SellingPrice, CostPrice){
-  if(SellingPrice > CostPrice){
-      console.log(`the way he is a great ${ds}`)
-  }
+function submitHandler(){
+    let ip = Number(initialPrice.value) ;
+    let qty = Number(quantityStocks.value);
+    let curr = Number(currentPrice.value) ;
+
+    calculateProfitAndLoss(ip, qty, curr);
 }
 
 
 
 
+function calculateProfitAndLoss(initial , quantity, current){
+if(initial > current){
+    let loss = (initial-current) * quantity;
+    let lossPercentage = (loss / initial) * 100;
+    console.log(`Hey the ${loss} is ${lossPercentage}`);
+}else if(current > initial){
+    let profit = (current - initial) * quantity
+    let profitPercentage = (profit / initial ) * 100
+    console.log(`Hey the ${profit} is ${profitPercentage}`);
+}else{
+    console.log(`No pain no gain , no gain no pain `);
+}
+}
+    
 
-submitBtn.addEventListener("click", calculateProfitAndLoss);
