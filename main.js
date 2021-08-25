aeebae65c8dfcbd39bf7c17dd64bea095f7288f6
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-24 23:09:53 -0400

    final commit

--- main.js
+++ main.js
@@ -2,35 +2,41 @@ const initialPrice = document.querySelector("#initial-price");
 const currentPrice = document.querySelector("#current-price");
 const quantityStocks = document.querySelector("quantity-stocks");
 const submitBtn = document.querySelector("#submit-btn");
+const outputBox = document.querySelector("#output");
 
+
+
+function submitHandler(){
+    var ip = Number(initialPrice.value) ;
+    var qty =  Number(quantityStocks.value);
+    var curr =  Number(currentPrice.value) ;
+
+
+ calculateProfitAndLoss(ip, qty, curr);
+}
 
 
 
 function calculateProfitAndLoss(initial, quantity, current){
-    if(initial > current){
-    let loss = (initial-current) * quantity;
-    let lossPercentage = (loss / initial) * 100;
-        console.log(`Hey the ${loss} is ${lossPercentage}`);
-    }else if(current > initial){
-        let profit = (current - initial) * quantity
-        let profitPercentage = (profit / initial ) * 100
-        console.log(`Hey the ${profit} is ${profitPercentage}`);
-    }else{
-        console.log(`No pain no gain , no gain no pain `);
-    }
-    }
-
-
-function submitHandler(){
-    var  ip = Number(initialPrice.value);
-    var  qty = Number(quantityStocks.value);
-    var  curr = Number(currentPrice.value);
-    calculateProfitAndLoss(ip, qty, curr);
-
+if(initial > current){
+        var loss = (initial - current) * quantity;
+        var lossPercentage = (loss / initial ) * 100 ;
+        outputBox.innerText = `Hey there is a ${loss} of loss and  this much of ${lossPercentage}% ` ;
+   }else if(current > initial ){
+        var profit = (current - initial) * quantity ;
+        var profitPercentage =(profit / initial) * 100 ;
+        outputBox.innerText = `Hey the is a profit of ${profit} and the percentage is ${profitPercentage}%`;
+   }else{
+    console.log(`No pain and no gain `);
+   }
 }
 
 
-submitBtn.addEventListener("click", submitHandler);
+submitBtn.addEventListener("click", submitHandler)
+
+
+
+
 
 
 
