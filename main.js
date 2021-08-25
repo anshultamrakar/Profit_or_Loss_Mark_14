47f1ef256671f47cd9b2fe5b340824d236fc3c55
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-24 21:44:15 -0400

    final commit

--- main.js
+++ main.js
@@ -5,31 +5,33 @@ const submitBtn = document.querySelector("#submit-btn");
 
 
 
-submitBtn.addEventListener("click", submitHandler);
 
-function submitHandler(){
-    let ip = Number(initialPrice.value) ;
-    let qty = Number(quantityStocks.value);
-    let curr = Number(currentPrice.value) ;
-
-    calculateProfitAndLoss(ip, qty, curr);
-}
-
-
-
-
-function calculateProfitAndLoss(initial , quantity, current){
-if(initial > current){
+function calculateProfitAndLoss(initial, quantity, current){
+    if(initial > current){
     let loss = (initial-current) * quantity;
     let lossPercentage = (loss / initial) * 100;
-    console.log(`Hey the ${loss} is ${lossPercentage}`);
-}else if(current > initial){
-    let profit = (current - initial) * quantity
-    let profitPercentage = (profit / initial ) * 100
-    console.log(`Hey the ${profit} is ${profitPercentage}`);
-}else{
-    console.log(`No pain no gain , no gain no pain `);
+        console.log(`Hey the ${loss} is ${lossPercentage}`);
+    }else if(current > initial){
+        let profit = (current - initial) * quantity
+        let profitPercentage = (profit / initial ) * 100
+        console.log(`Hey the ${profit} is ${profitPercentage}`);
+    }else{
+        console.log(`No pain no gain , no gain no pain `);
+    }
+    }
+
+
+function submitHandler(){
+    var  ip = Number(initialPrice.value);
+    var  qty = Number(quantityStocks.value);
+    var  curr = Number(currentPrice.value);
+    calculateProfitAndLoss(ip, qty, curr);
+
 }
-}
-    
+
+
+submitBtn.addEventListener("click", submitHandler);
+
+
+
 
