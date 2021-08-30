70f7a61c8246a8fb88556311b46b7648e9c774c1
anshultamrakar <anshultamrakar487@gmail.com>
2021-08-30 02:41:46 -0400

    fix bug
    Completed

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const initialPrice = document.querySelector("#initial-price");
 const currentPrice = document.querySelector("#current-price");
-const quantityStocks = document.querySelector("quantity-stocks");
+const quantityStocks = document.querySelector("#quantity-stocks");
 const submitBtn = document.querySelector("#submit-btn");
 const outputBox = document.querySelector("#output");
 
-
+submitBtn.addEventListener("click", submitHandler);
 
 function submitHandler(){
-    var ip = Number(initialPrice.value) ;
-    var qty =  Number(quantityStocks.value);
-    var curr =  Number(currentPrice.value) ;
-     calculateProfitAndLoss(500,1000,10000);
+    var ip = parseInt(initialPrice.value) ;
+    var qtyyy =  parseInt(quantityStocks.value);
+    var curr =  parseInt(currentPrice.value) ;
+    calculateProfitAndLoss(ip,qtyyy,curr);
 
 }
-
 
 
 function calculateProfitAndLoss(initial, quantity, current){
@@ -24,14 +23,14 @@ if(initial > current){
    }else if(current > initial ){
         var profit = (current - initial) * quantity ;
         var profitPercentage =(profit / initial) * 100 ;
-        outputBox.innerText = `Hey the is a profit of ${profit} and the percentage is ${profitPercentage}%`;
+        outputBox.innerText = `Hey there is a profit of ${profit} and the percentage is ${profitPercentage}%`;
    }else{
     console.log(`No pain and no gain `);
    }
 }
 
 
-submitBtn.addEventListener("click", submitHandler)
+
 
 
 
